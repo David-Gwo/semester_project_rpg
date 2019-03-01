9547cf518374c26e3b71b81b1864cd98f2aac8af
Guillem <gtorrent@everis.com>
2019-03-01 14:35:38 +0100

    Add example cnn model and mnist training script

---
+++ mnist_cnn_example.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import numpy as np
+
+from cnn_model import cnn_model_fn
+
+# Load training and eval data
+((train_data, train_labels), (eval_data, eval_labels)) = tf.keras.datasets.mnist.load_data()
+
+train_data = train_data/np.float32(255)
+train_labels = train_labels.astype(np.int32)  # not required
+
+eval_data = eval_data/np.float32(255)
+eval_labels = eval_labels.astype(np.int32)  # not required
+
+# Create the Estimator
+mnist_classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
+
+# Set up logging for predictions
+tensors_to_log = {"probabilities": "softmax_tensor"}
+
+logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=50)
+
+# Train the model
+train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"x": train_data},
+    y=train_labels,
+    batch_size=100,
+    num_epochs=None,
+    shuffle=True)
+
+# train one step and display the probabilties
+mnist_classifier.train(
+    input_fn=train_input_fn,
+    steps=1000,
+    hooks=[logging_hook])
