Fix quaternion normalization in plot evaluation

data/euroc_manager.py
@@ -243,7 +243,8 @@ def generate_tf_imu_train_ds(euroc_dir, euroc_train, batch_s, trained_model_dir,
     return train_ds, val_ds, (train_ds_len, val_ds_len)
 
 
-def generate_tf_imu_test_ds(euroc_dir, euroc_test, batch_s, trained_model_dir, window_len, normalize=True):
+def generate_tf_imu_test_ds(euroc_dir, euroc_test, batch_s, trained_model_dir, window_len, normalize=True,
+                            full_batches=False):
     """
     Read the preprocessed euroc dataset from saved file. Generate the tf-compatible testing dataset
 
@@ -253,6 +254,7 @@ def generate_tf_imu_test_ds(euroc_dir, euroc_test, batch_s, trained_model_dir, w
     :param trained_model_dir: Name of the directory where trained model is stored
     :param window_len: length of the sampling window
     :param normalize: whether data should be normalized using scaler factors
+    :param full_batches: whether batches should all be the same size
     :return: the tf-compatible testing dataset, and its length
     """
 
@@ -274,7 +276,7 @@ def generate_tf_imu_test_ds(euroc_dir, euroc_test, batch_s, trained_model_dir, w
     ds_len = len(gt_v_tensor)
 
     test_ds = tf.data.Dataset.from_tensor_slices((imu_img_tensor, gt_v_tensor))
-    test_ds = test_ds.batch(batch_s)
+    test_ds = test_ds.batch(batch_s, drop_remainder=full_batches)
 
     return test_ds, ds_len
 

data/utils/data_utils.py
@@ -7,10 +7,11 @@ import logging
 import requests
 import scipy.io
 import numpy as np
+from pyquaternion import Quaternion
 from matplotlib import pyplot as plt
 from scipy import signal
 from scipy.interpolate import interp1d
-from utils import quaternion_error
+from utils import quaternion_error, unit_quat
 
 from tensorflow.python.keras.preprocessing.image import Iterator
 from tensorflow.python.keras import datasets as k_ds
@@ -440,18 +441,22 @@ def plot_prediction(gt, prediction, manual_pred):
         ax3 = fig3.add_subplot(4, 1, 3)
         ax4 = fig3.add_subplot(4, 1, 4)
 
-        ax1.plot(gt[:, 6], 'b')
-        ax1.plot(prediction[:, 6], 'r')
-        ax1.plot(manual_pred[:, 6], 'k')
-        ax2.plot(gt[:, 7], 'b')
-        ax2.plot(prediction[:, 7], 'r')
-        ax2.plot(manual_pred[:, 7], 'k')
-        ax3.plot(gt[:, 8], 'b')
-        ax3.plot(prediction[:, 8], 'r')
-        ax3.plot(manual_pred[:, 8], 'k')
-        ax4.plot(gt[:, 9], 'b')
-        ax4.plot(prediction[:, 9], 'r')
-        ax4.plot(manual_pred[:, 9], 'k')
+        gt_q = np.array([unit_quat(gt[i, 6:]).elements for i in range(len(gt))])
+        pred_q = np.array([unit_quat(prediction[i, 6:]).elements for i in range(len(gt))])
+        mpred_q = np.array([unit_quat(manual_pred[i, 6:]).elements for i in range(len(gt))])
+
+        ax1.plot(gt_q[:, 0], 'b')
+        ax1.plot(pred_q[:, 0], 'r')
+        ax1.plot(mpred_q[:, 0], 'k')
+        ax2.plot(gt_q[:, 1], 'b')
+        ax2.plot(pred_q[:, 1], 'r')
+        ax2.plot(mpred_q[:, 1], 'k')
+        ax3.plot(gt_q[:, 2], 'b')
+        ax3.plot(pred_q[:, 2], 'r')
+        ax3.plot(mpred_q[:, 2], 'k')
+        ax4.plot(gt_q[:, 3], 'b')
+        ax4.plot(pred_q[:, 3], 'r')
+        ax4.plot(mpred_q[:, 3], 'k')
         ax1.set_title('att_w')
         ax2.set_title('att_x')
         ax3.set_title('att_y')

models/base_learner.py
@@ -4,7 +4,6 @@ import math
 import numpy as np
 import tensorflow as tf
 
-from tensorflow.python.summary import summary as tf_summary
 from tensorflow.python.keras import callbacks
 from tensorflow.python.keras.optimizers import Adam
 
@@ -136,44 +135,6 @@ class Learner(object):
                                           self.config.prepared_train_data_file, self.config.prepared_test_data_file,
                                           self.config.prepared_file_available, self.trained_model_dir)
 
-    def collect_summaries(self):
-        """Collects all summaries to be shown in the tensorboard"""
-
-        #######################################################
-        # ADD HERE THE VARIABLES YOU WANT TO SEE IN THE BOARD #
-        #######################################################
-        tf_summary.scalar("train_loss", self.total_loss, collections=["step_sum"])
-        tf_summary.scalar("accuracy", self.accuracy, collections=["step_sum"])
-        tf_summary.histogram("logits_distribution", self.logits, collections=["step_sum"])
-        tf_summary.histogram("predicted_out_distributions", tf.argmax(self.logits, 1), collections=["step_sum"])
-        tf_summary.histogram("ground_truth_distribution", self.labels, collections=["step_sum"])
-
-        ###################################################
-        # LEAVE UNCHANGED (gradients and tensors summary) #
-        ###################################################
-        for var in tf.compat.v1.trainable_variables():
-            tf_summary.histogram(var.op.name + "/values", var, collections=["step_sum"])
-        for grad, var in self.grads_and_vars:
-            tf_summary.histogram(var.op.name + "/gradients", grad, collections=["step_sum"])
-        self.step_sum = tf.summary.merge(tf.compat.v1.get_collection('step_sum'))
-
-        ####################
-        # VALIDATION ERROR #
-        ####################
-        self.validation_loss = tf.compat.v1.placeholder(tf.float32, [])
-        self.validation_accuracy = tf.compat.v1.placeholder(tf.float32, [])
-        tf_summary.scalar("Validation_Loss", self.validation_loss, collections=["validation_summary"])
-        tf_summary.scalar("Validation_Accuracy", self.validation_accuracy, collections=["validation_summary"])
-        self.val_sum = tf_summary.merge(tf.compat.v1.get_collection('validation_summary'))
-
-    def save(self, sess, checkpoint_dir, step):
-        model_name = 'model'
-        print(" [*] Saving checkpoint to {}/model-{}".format(checkpoint_dir, step))
-        if step == 'best':
-            self.saver.save(sess, os.path.join(checkpoint_dir, model_name + '.best'))
-        else:
-            self.saver.save(sess, os.path.join(checkpoint_dir, model_name), global_step=step)
-
     def train(self):
         self.build_and_compile_model()
 
@@ -226,7 +187,7 @@ class Learner(object):
         #     self.custom_backprop(val_ds[0], val_ds[0], (val_ds_splits[0], val_ds_splits[0]), epoch)
 
         # Train!
-        history = self.regressor_model.fit(
+        self.regressor_model.fit(
             train_ds,
             verbose=2,
             epochs=self.config.max_epochs,
@@ -276,7 +237,7 @@ class Learner(object):
         if self.config.generate_training_progression:
             dataset = self.config.prepared_train_data_file
 
-        dataset = self.config.prepared_train_data_file
+        # dataset = self.config.prepared_train_data_file
         if testing_ds is None:
             # TODO: make more elegant
             if self.config.test_ds == 'blackbird':
@@ -290,7 +251,7 @@ class Learner(object):
                                                      self.config.batch_size,
                                                      self.config.checkpoint_dir + self.model_version_number,
                                                      self.config.window_length)
-            steps = steps / self.config.batch_size
+            steps = np.floor(steps / self.config.batch_size)
         else:
             test_ds = testing_ds.take(steps)
 
@@ -303,12 +264,15 @@ class Learner(object):
                 test_dir = bb_manager.ds_local_dir
             else:
                 test_dir = self.config.test_dir
+
+            # Generate un-normalized dataset for manual integration
             test_ds, steps = generate_tf_imu_test_ds(test_dir,
                                                      dataset,
                                                      self.config.batch_size,
                                                      self.config.checkpoint_dir + self.model_version_number,
                                                      self.config.window_length,
-                                                     normalize=False)
+                                                     normalize=False,
+                                                     full_batches=True)
             manual_predictions = imu_integration(test_ds, self.config.window_length)
         else:
             manual_predictions = None
@@ -317,28 +281,3 @@ class Learner(object):
             plot_regression_predictions(test_ds, predictions, epoch=self.last_epoch_number, i=fig_n)
         else:
             plot_regression_predictions(test_ds, predictions, manual_pred=manual_predictions)
-
-    def epoch_end_callback(self, sess, sv, epoch_num):
-        # Evaluate val accuracy
-        val_loss = 0
-        val_accuracy = 0
-        for i in range(self.val_steps_per_epoch):
-            loss, accuracy = sess.run([self.total_loss, self.accuracy],
-                             feed_dict={self.is_training: False})
-            val_loss+= loss
-            val_accuracy += accuracy
-        val_loss = val_loss / self.val_steps_per_epoch
-        val_accuracy = val_accuracy / self.val_steps_per_epoch
-        # Log to Tensorflow board
-        val_sum = sess.run(self.val_sum, feed_dict ={
-            self.validation_loss: val_loss,
-            self.validation_accuracy: val_accuracy})
-        sv.summary_writer.add_summary(val_sum, epoch_num)
-        print("Epoch [{}] Validation Loss: {} Validation Accuracy: {}".format(
-            epoch_num, val_loss, val_accuracy))
-        # Model Saving
-        if val_loss < self.min_val_loss:
-            self.save(sess, self.config.checkpoint_dir, 'best')
-            self.min_val_loss = val_loss
-        if epoch_num % self.config.save_freq == 0:
-            self.save(sess, self.config.checkpoint_dir, epoch_num)

utils.py
@@ -74,6 +74,15 @@ def rotate_quat(q1, q2):
     return q2 * q1.inverse
 
 
+def unit_quat(q):
+    if len(np.shape(q)) == 2:
+        return [Quaternion(q_i).unit for q_i in q]
+    elif len(np.shape(q)) == 1:
+        return Quaternion(q).unit
+    else:
+        TypeError("input should be a 4 component array or an nx4 numpy matrix")
+
+
 def quaternion_error(quat_1, quat_2, normalize=True):
     """
     Calculates the quaternion that rotates quaternion quat_1 to quaternion quat_2, or element-wise if given two lists of
@@ -87,12 +96,12 @@ def quaternion_error(quat_1, quat_2, normalize=True):
 
     if len(np.shape(quat_1)) == len(np.shape(quat_1)) == 2:
         if normalize:
-            q_pred_e = [rotate_quat(Quaternion(quat_1[i]).unit, Quaternion(quat_2[i]).unit) for i in range(len(quat_1))]
+            q_pred_e = [rotate_quat(unit_quat(quat_1[i]), unit_quat(quat_2[i])) for i in range(len(quat_1))]
         else:
             q_pred_e = [rotate_quat(Quaternion(quat_1[i]), Quaternion(quat_2[i])) for i in range(len(quat_1))]
     elif len(np.shape(quat_1)) == len(np.shape(quat_1)) == 1:
         if normalize:
-            q_pred_e = rotate_quat(Quaternion(quat_1).unit, Quaternion(quat_2).unit)
+            q_pred_e = rotate_quat(unit_quat(quat_1), unit_quat(quat_2))
         else:
             q_pred_e = rotate_quat(Quaternion(quat_1), Quaternion(quat_2))
     else: