Fix model bugs. Fix l2_loss bug. Fix custom backprop

models/base_learner.py
@@ -10,6 +10,7 @@ from models.nets import pre_integration_net as prediction_network
 from models.customized_tf_funcs.custom_callbacks import CustomModelCheckpoint
 from models.customized_tf_funcs.custom_losses import *
 from models.test_experiments import ExperimentManager
+from tensorflow.python.keras.utils import plot_model
 
 #############################################################################
 # IMPORT HERE A LIBRARY TO PRODUCE ALL THE FILENAMES (and optionally labels)#
@@ -45,10 +46,21 @@ class Learner(object):
 
             with tf.GradientTape() as tape:
                 # Forward pass
-                logit = self.trainable_model(x)
+                predictions = self.trainable_model(x)
+                predictions = {out.name.split(':')[0]: predictions[i] for i, out in enumerate(
+                    self.trainable_model.outputs)}
+                predictions = {i.split('/')[0]: predictions[i] for i in predictions.keys()}
 
                 # External loss calculation
-                loss = so3_loss_func(y, logit)
+                loss_connections = {"state_output": mock_loss,
+                                    "pre_integrated_R": l2_loss,
+                                    "pre_integrated_v": l2_loss,
+                                    "pre_integrated_p": l2_loss}
+                loss = []
+                for key in loss_connections.keys():
+                    loss.append(loss_connections[key](y[key], predictions[key]))
+
+                loss = tf.add_n([loss_i for loss_i in loss])
 
                 # Manual loss combination:
                 loss += sum(self.trainable_model.losses)
@@ -67,11 +79,12 @@ class Learner(object):
         trainable_model = prediction_network(window_len=self.config.window_length)
 
         print(trainable_model.summary())
+        plot_model(trainable_model, to_file='model.png')
 
         loss_connections = {"state_output": mock_loss,
-                            "pre_integrated_R": l1_loss,
-                            "pre_integrated_v": l1_loss,
-                            "pre_integrated_p": l1_loss}
+                            "pre_integrated_R": l2_loss,
+                            "pre_integrated_v": l2_loss,
+                            "pre_integrated_p": l2_loss}
 
         if not is_testing:
             loss_weight = {'state_output': 1,
@@ -83,10 +96,7 @@ class Learner(object):
                                     loss_weight=loss_weight)
         else:
             trainable_model.compile(optimizer=tf.keras.optimizers.Adam(self.config.learning_rate, self.config.beta1),
-                                    loss={"state_output": mock_loss,
-                                          "pre_integrated_R": l1_loss,
-                                          "pre_integrated_v": l1_loss,
-                                          "pre_integrated_p": l1_loss})
+                                    loss=loss_connections)
 
         self.trainable_model = trainable_model
 

models/customized_tf_funcs/custom_layers.py
@@ -9,6 +9,7 @@ from tensorflow.python.keras import backend as K
 from utils.algebra import exp_mapping, apply_state_diff, rotate_quat, rotate_vec
 
 import numpy as np
+import tensorflow as tf
 
 
 class ForkLayerIMUdt(Layer):
@@ -40,20 +41,17 @@ class PreIntegrationForwardDense(Layer):
         if len(target_shape) != 2:
             raise ValueError("The target shape should be 3D")
 
-        self.units = int(target_shape[0])
-        self.channels = int(target_shape[1])
+        self.target_shape = target_shape
         self.activation = activations.get(activation)
         self.use_bias = use_bias
         self.kernel_initializer = initializers.get(kernel_initializer)
         self.bias_initializer = initializers.get(bias_initializer)
 
-        self.feature_kernel = None
         self.bias = None
-        self.recurrent_mask = None
-        self.recurrent_kernel = None
+        self.mask = None
+        self.kernel = None
 
-        self.feature_units = None
-        self.recurrent_units = None
+        self.flat_length = None
 
     def build(self, input_shape):
         dtype = dtypes.as_dtype(self.dtype or K.floatx())
@@ -65,37 +63,28 @@ class PreIntegrationForwardDense(Layer):
             raise ValueError('The last dimension of the inputs to `Dense` should be defined. Found `None`.')
         if len(input_shape) != 3:
             raise ValueError('The input shape should be a 3D tensor. Found %s' % (input_shape,))
-        if input_shape[1] != self.units:
-            raise ValueError('The first dimension of input and output shape must coincide')
-        if input_shape[2] < self.channels:
-            raise ValueError('The number of channels in the input is smaller than in the output. It should be at least'
-                             'the same size')
+        if input_shape[1:3] != self.target_shape:
+            raise ValueError('The input and output shapes must coincide. Got {0} and {1}'.format(
+                input_shape[1:3], self.target_shape))
 
-        self.feature_units = input_shape[1] * (input_shape[2] - self.channels)
-        self.recurrent_units = input_shape[1] * self.channels
+        self.flat_length = np.prod(input_shape[1:3])
+        output_channels = self.target_shape[-1]
 
-        self.feature_kernel = self.add_weight(
+        self.kernel = self.add_weight(
             'feature_kernel',
-            shape=[self.feature_units, self.recurrent_units],
+            shape=[self.flat_length, self.flat_length],
             initializer=self.kernel_initializer,
             dtype=self.dtype,
             trainable=True)
 
-        self.recurrent_kernel = self.add_weight(
-            'recurrent_kernel',
-            shape=[self.recurrent_units, self.recurrent_units],
-            initializer=self.kernel_initializer,
-            dtype=self.dtype,
-            trainable=True)
-
-        recurrent_mask = np.zeros((self.units, self.units))
-        for i in range(self.units):
-            for j in range(i, self.units):
+        recurrent_mask = np.zeros((input_shape[1], input_shape[1]))
+        for i in range(input_shape[1]):
+            for j in range(i, input_shape[1]):
                 recurrent_mask[i, j] = 1
-        recurrent_mask = np.tile(recurrent_mask, (self.channels, self.channels))
+        recurrent_mask = np.tile(recurrent_mask[::-1, :], (output_channels, output_channels))
 
-        self.recurrent_mask = self.add_weight(
-            shape=[self.recurrent_units, self.recurrent_units],
+        self.mask = self.add_weight(
+            shape=[self.flat_length, self.flat_length],
             initializer=lambda shape, dtype, partition_info=None: K.variable(recurrent_mask),
             dtype=self.dtype,
             trainable=False)
@@ -103,42 +92,37 @@ class PreIntegrationForwardDense(Layer):
         if self.use_bias:
             self.bias = self.add_weight(
                 'bias',
-                shape=[self.recurrent_units, ],
+                shape=[self.flat_length, ],
                 initializer=self.bias_initializer,
                 dtype=self.dtype,
                 trainable=True)
 
         self.built = True
 
+    # @tf.function
     def call(self, inputs, **kwargs):
 
         inputs = ops.convert_to_tensor(inputs)
 
-        feature_vec = inputs[:, :, :-self.channels]
-        recurrent_vec = inputs[:, :, -self.channels:]
+        if not inputs.shape[0]:
+            return K.expand_dims(inputs, axis=3)
 
-        if not context.executing_eagerly():
-            return recurrent_vec
-
-        feature_vec = K.reshape(feature_vec, (feature_vec.shape[0], self.units*feature_vec.shape[2]))
-        recurrent_vec = K.reshape(recurrent_vec, (recurrent_vec.shape[0], self.units * self.channels))
+        vectorized_input = K.reshape(inputs, (inputs.shape[0], self.flat_length))
 
         # Cast the inputs to self.dtype, which is the variable dtype. We do not
         # cast if `should_cast_variables` is True, as in that case the variable
         # will be automatically casted to inputs.dtype.
         if not self._mixed_precision_policy.should_cast_variables:
-            feature_vec = math_ops.cast(feature_vec, self.dtype)
-            recurrent_vec = math_ops.cast(recurrent_vec, self.dtype)
+            vectorized_input = math_ops.cast(vectorized_input, self.dtype)
 
-        outputs = gen_math_ops.mat_mul(feature_vec, self.feature_kernel)
-        outputs += gen_math_ops.mat_mul(gen_math_ops.mat_mul(recurrent_vec, self.recurrent_kernel), self.recurrent_mask)
+        outputs = gen_math_ops.mat_mul(vectorized_input, tf.math.multiply(self.kernel, self.mask))
 
         if self.use_bias:
             outputs = nn.bias_add(outputs, self.bias)
         if self.activation is not None:
             outputs = self.activation(outputs)  # pylint: disable=not-callable
 
-        return K.reshape(outputs, (outputs.shape[0], self.units, self.channels))
+        return K.reshape(outputs, (outputs.shape[0], self.target_shape[0], self.target_shape[1], 1))
 
 
 class ExponentialRemappingLayer(Layer):

models/customized_tf_funcs/custom_losses.py
@@ -14,7 +14,7 @@ def l1_loss(y_true, y_pred):
 
 
 def l2_loss(y_true, y_pred):
-    abs_diff = tf.sqrt(tf.math.subtract(tf.cast(y_true, tf.float32), y_pred)**2)
+    abs_diff = tf.math.subtract(tf.cast(y_true, tf.float32), y_pred)**2
 
     while len(abs_diff.shape) > 1:
         abs_diff = tf.reduce_sum(abs_diff, axis=1)
@@ -23,7 +23,9 @@ def l2_loss(y_true, y_pred):
 
 
 def mock_loss(y_true, _):
-    return y_true * 0
+    if not y_true.shape[0]:
+        return y_true
+    return tf.zeros(y_true.shape[0])
 
 
 def so3_loss_func(y_true, y_pred):

models/nets.py
@@ -1,26 +1,25 @@
 from tensorflow.python.keras import regularizers, Sequential
 from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.layers import Dense, Activation, Flatten, Input, Conv2D, MaxPooling2D, Concatenate, \
-    Reshape, Permute
+from tensorflow.python.keras import layers
 
 from models.customized_tf_funcs.custom_layers import ExponentialRemappingLayer, ForkLayerIMUdt, DiffConcatenationLayer, \
     ReshapeIMU, PreIntegrationForwardDense, FinalPreIntegration, IntegratingLayer
 
+import tensorflow as tf
 import numpy as np
 
 
 def vel_cnn():
     model = Sequential()
 
-    model.add(Conv2D(filters=60, kernel_size=(3, 6), padding='same', activation='relu', input_shape=(200, 6, 1),
-                     name="convolution_layer_1"))
-    model.add(Conv2D(filters=120, kernel_size=(3, 6), padding='same', activation='relu', name="convolution_layer_2"))
-    model.add(Conv2D(filters=240, kernel_size=(3, 1), padding='valid', activation='relu', name="convolution_layer_3"))
-    model.add(MaxPooling2D(pool_size=(10, 1), strides=(6, 1), name="pooling_layer"))
-    model.add(Flatten(name="flattening_layer"))
-    model.add(Dense(400, activation='relu', name="dense_layer_1"))
-    model.add(Dense(100, activation='relu', name="dense_layer_2"))
-    model.add(Dense(1, name="output_layer"))
+    model.add(layers.Conv2D(filters=60, kernel_size=(3, 6), padding='same', activation='relu', input_shape=(200, 6, 1)))
+    model.add(layers.Conv2D(filters=120, kernel_size=(3, 6), padding='same', activation='relu'))
+    model.add(layers.Conv2D(filters=240, kernel_size=(3, 1), padding='valid', activation='relu'))
+    model.add(layers.MaxPooling2D(pool_size=(10, 1), strides=(6, 1)))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(400, activation='relu'))
+    model.add(layers.Dense(100, activation='relu'))
+    model.add(layers.Dense(1))
 
     return model
 
@@ -34,41 +33,41 @@ def imu_so3_integration_net(window_len):
     conv_kernel_width = min([window_len, 2])
 
     # Input layers
-    net_in = Input((window_len + input_state_len, 7, 1), name="imu_input")
-    state_0 = Input((input_state_len, 1), name="state_input")
+    net_in = layers.Input((window_len + input_state_len, 7, 1), name="imu_input")
+    state_0 = layers.Input((input_state_len, 1), name="state_input")
 
     imu_stack, time_diff_imu = ForkLayerIMUdt(name="forking_layer")(net_in)
 
-    imu_conv_1 = Conv2D(filters=15, kernel_size=(conv_kernel_width, 3), strides=(1, 3), padding='same',
-                        activation='relu', name='imu_conv_layer_1')(imu_stack)
-    imu_conv_2 = Conv2D(filters=30, kernel_size=(conv_kernel_width, 1), padding='same',
-                        activation='relu', name='imu_conv_layer_2')(imu_conv_1)
-    imu_conv_3 = Conv2D(filters=60, kernel_size=(conv_kernel_width, 1), padding='same',
-                        activation='relu', name='imu_conv_layer_3')(imu_conv_2)
+    imu_conv_1 = layers.Conv2D(filters=15, kernel_size=(conv_kernel_width, 3), strides=(1, 3), padding='same',
+                               activation='relu', name='imu_conv_layer_1')(imu_stack)
+    imu_conv_2 = layers.Conv2D(filters=30, kernel_size=(conv_kernel_width, 1), padding='same',
+                               activation='relu', name='imu_conv_layer_2')(imu_conv_1)
+    imu_conv_3 = layers.Conv2D(filters=60, kernel_size=(conv_kernel_width, 1), padding='same',
+                               activation='relu', name='imu_conv_layer_3')(imu_conv_2)
 
-    imu_conv_reduced = Conv2D(filters=imu_final_channels, kernel_size=(conv_kernel_width, 1), padding='same',
-                              activation='tanh', name='imu_1x_conv_layer')(imu_conv_3)
-    imu_conv_flat = Reshape((window_len, imu_final_channels * 2, 1), name='reshape_layer')(imu_conv_reduced)
-    re_stacked_imu = Concatenate(name='imu_concatenating_layer', axis=2)([imu_conv_flat, time_diff_imu])
+    imu_conv_reduced = layers.Conv2D(filters=imu_final_channels, kernel_size=(conv_kernel_width, 1), padding='same',
+                                     activation='tanh', name='imu_1x_conv_layer')(imu_conv_3)
+    imu_conv_flat = layers.Reshape((window_len, imu_final_channels * 2, 1), name='reshape_layer')(imu_conv_reduced)
+    re_stacked_imu = layers.Concatenate(name='imu_concatenating_layer', axis=2)([imu_conv_flat, time_diff_imu])
 
-    imu_ts_conv = Conv2D(filters=1+2*imu_final_channels, kernel_size=(conv_kernel_width, 1+2*imu_final_channels),
-                         padding='valid', activation='relu', name='imu_final_conv_layer')(re_stacked_imu)
+    imu_ts_conv = layers.Conv2D(filters=1+2*imu_final_channels, kernel_size=(conv_kernel_width, 1+2*imu_final_channels),
+                                padding='valid', activation='relu', name='imu_final_conv_layer')(re_stacked_imu)
 
-    flatten_conv_imu = Flatten(name='imu_flattening_layer')(imu_ts_conv)
-    flatten_state_0 = Flatten(name='state_input_flattening_layer')(state_0)
+    flatten_conv_imu = layers.Flatten(name='imu_flattening_layer')(imu_ts_conv)
+    flatten_state_0 = layers.Flatten(name='state_input_flattening_layer')(state_0)
 
-    stacked = Concatenate(name='data_concatenating_layer')([flatten_conv_imu, flatten_state_0])
+    stacked = layers.Concatenate(name='data_concatenating_layer')([flatten_conv_imu, flatten_state_0])
 
-    dense_1 = Dense(400, name='dense_layer_1')(stacked)
-    activation_1 = Activation('relu', name='activation_1')(dense_1)
+    dense_1 = layers.Dense(400, name='dense_layer_1')(stacked)
+    activation_1 = layers.Activation('relu', name='activation_1')(dense_1)
 
-    dense_2 = Dense(200, name='dense_layer_2')(activation_1)
-    activation_2 = Activation('relu', name='activation_2')(dense_2)
+    dense_2 = layers.Dense(200, name='dense_layer_2')(activation_1)
+    activation_2 = layers.Activation('relu', name='activation_2')(dense_2)
 
-    dense_3 = Dense(100, name='dense_layer_3')(activation_2)
-    activation_3 = Activation('relu', name='activation_3')(dense_3)
+    dense_3 = layers.Dense(100, name='dense_layer_3')(activation_2)
+    activation_3 = layers.Activation('relu', name='activation_3')(dense_3)
 
-    net_out = Dense(output_state_len, name='state_output')(activation_3)
+    net_out = layers.Dense(output_state_len, name='state_output')(activation_3)
 
     state_out = ExponentialRemappingLayer(name='remapped_state_output')(net_out)
     return Model(inputs=net_in, outputs=net_out), Model(inputs=net_in, outputs=state_out)
@@ -77,23 +76,24 @@ def imu_so3_integration_net(window_len):
 def pre_integration_net(window_len):
 
     # Define parameters for model
-    kernel_width = min([window_len, 2])
+    kernel_width = min([window_len, 3])
     pooling_width = min([window_len, 2])
 
     input_state_shape = (10,)
     pre_integration_shape = (window_len, 3)
-
     imu_input_shape = (window_len, 7, 1)
 
     # This parameter will vary in terms of window_len (higher window_len will allow more layers)
-    n_conv_layers = 4
+    n_conv_layers = 3
 
     # Input layers. Don't change names
-    imu_in = Input(imu_input_shape, name="imu_input")
-    state_in = Input(input_state_shape, name="state_input")
+    imu_in = layers.Input(imu_input_shape, name="imu_input")
+    state_in = layers.Input(input_state_shape, name="state_input")
 
     # Pre-processing
-    x = ReshapeIMU()(imu_in)
+    x_shrink = pre_integration_shape[0] - 8 * round(pre_integration_shape[0]/8) + 1
+    imu_reshaped = layers.Conv2D(1, kernel_size=(x_shrink, 1))(imu_in)
+    x = ReshapeIMU()(imu_reshaped)
     _, dt_vec = ForkLayerIMUdt()(imu_in)
 
     #############################
@@ -101,7 +101,7 @@ def pre_integration_net(window_len):
     #############################
 
     # Convolution layers
-    for i in range(n_conv_layers):
+    def down_scaling_loop(inputs, iterations, i):
         if i == 0:
             kernel_size = (kernel_width, 4)
             strides = (1, 4)
@@ -109,28 +109,49 @@ def pre_integration_net(window_len):
             kernel_size = (kernel_width, 1)
             strides = (1, 1)
 
-        x = Conv2D(window_len * (i + 1), kernel_size=kernel_size, strides=strides, padding='same', activation='relu')(x)
-        x = MaxPooling2D(pool_size=(pooling_width, 1))(x)
+        x1 = layers.Conv2D(window_len * (i + 1), kernel_size=kernel_size, strides=strides, padding='same',
+                           activation='relu')(inputs)
+        x2 = layers.Conv2D(window_len * (i + 1), kernel_size=kernel_size, padding='same', activation='relu')(x1)
+        x3 = layers.Conv2D(window_len * (i + 1), kernel_size=kernel_size, padding='same', activation='relu')(x2)
 
-    final_length = np.floor(window_len / pooling_width ** n_conv_layers)
+        if iterations > 0:
+
+            x_up = layers.MaxPooling2D(pool_size=(pooling_width, 1))(tf.add(x1, x3))
+            x3 = layers.Conv2D(window_len * (i + iterations + 1), kernel_size=kernel_size, padding='same', activation='relu')(x3)
+
+            x_up = down_scaling_loop(x_up, iterations - 1, i + 1)
+            x_up = layers.UpSampling2D(size=(pooling_width, 1))(x_up)
+
+            x3 = tf.add(x3, x_up)
+
+        return x3
+
+    x = down_scaling_loop(x, n_conv_layers, 0)
 
     # Adapt feature vector so it has a compatible shape with the outputs
-    x = Permute(dims=[3, 1, 2])(x)
-    x = Conv2D(n_conv_layers, kernel_size=(4, 1), strides=(n_conv_layers, 1), padding='same', activation='relu')(x)
-    feat_vec = Reshape(target_shape=(window_len, n_conv_layers * final_length))(x)
-    x = Flatten()(feat_vec)
+    y_shrink = pre_integration_shape[0] - x.shape[1]
+    feat_vec = layers.Conv2DTranspose(pre_integration_shape[0], (x_shrink, y_shrink))(x)
 
     # Pre-integrated rotation
-    x = Dense(np.prod(pre_integration_shape), activation='relu')(x)
-    pre_integrated_rot = Reshape(target_shape=pre_integration_shape, name="pre_integrated_R")(x)
+    x = layers.Conv2D(1, kernel_size=(1, 1), activation='relu')(feat_vec)
+    pre_integrated_rot = tf.squeeze(x, axis=3, name="pre_integrated_R")
 
-    # Pre-integrated velocity
-    x = Concatenate(axis=2)([feat_vec, pre_integrated_rot])
-    pre_integrated_v = PreIntegrationForwardDense(pre_integration_shape, activation='relu', name="pre_integrated_v")(x)
+    # # Pre-integrated velocity
+    x = layers.Conv2D(1, kernel_size=(1, 1), activation='relu')(feat_vec)
+    y = PreIntegrationForwardDense(pre_integration_shape, activation='relu')(pre_integrated_rot)
+    x = layers.Concatenate(axis=-1)([x, y])
+    x = layers.Conv2D(window_len, kernel_size=(2, 2), padding='same')(x)
+    x = layers.Conv2D(1, kernel_size=(2, 2), padding='same')(x)
+    pre_integrated_v = tf.squeeze(x, axis=3, name="pre_integrated_v")
 
     # Pre-integrated position
-    x = Concatenate(axis=2)([feat_vec, pre_integrated_v])
-    pre_integrated_p = PreIntegrationForwardDense(pre_integration_shape, activation='relu', name="pre_integrated_p")(x)
+    x = layers.Conv2D(1, kernel_size=(1, 1), activation='relu')(feat_vec)
+    y = PreIntegrationForwardDense(pre_integration_shape, activation='relu')(pre_integrated_rot)
+    z = PreIntegrationForwardDense(pre_integration_shape, activation='relu')(pre_integrated_v)
+    x = layers.Concatenate(axis=-1)([x, y, z])
+    x = layers.Conv2D(window_len, kernel_size=(2, 2), padding='same')(x)
+    x = layers.Conv2D(1, kernel_size=(2, 2), padding='same')(x)
+    pre_integrated_p = tf.squeeze(x, axis=3, name="pre_integrated_p")
 
     #################################
     # ##  NON-TRAINABLE NETWORK  ## #